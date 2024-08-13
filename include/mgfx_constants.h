eccb74d16a67a7f6daf4d76975428b3fd35df418
Dennis Heinze <dennisjp.heinze@gmail.com>
2024-08-13 14:37:48 +0200

    remove some unneeded constants

--- include/mgfx_constants.h
+++ include/mgfx_constants.h
@@ -24,13 +24,6 @@
 
 #define MGFX_MATRIX_SIZE        64
 
-#define MGFX_VTX_POS            0
-#define MGFX_VTX_NORM           6
-#define MGFX_VTX_RGBA           8
-#define MGFX_VTX_TEX            12
-
-#define MGFX_VTX_MAX_SIZE       16
-
 #define MGFX_VTX_POS_SHIFT      5
 
 #endif
