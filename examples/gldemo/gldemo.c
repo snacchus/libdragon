a378aca8c8915476b1e3fc5755aa23504f0eb41e
Dennis Heinze <dennis.heinze@mailbox.org>
2026-05-14 15:28:03 +0200

    WIP

--- examples/gldemo/gldemo.c
+++ examples/gldemo/gldemo.c
@@ -2,330 +2,258 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/gl_integration.h>
-#include <rspq_profile.h>
-#include <malloc.h>
-#include <math.h>
 
-#include "camera.h"
-#include "cube.h"
-#include "decal.h"
-#include "sphere.h"
-#include "plane.h"
-#include "prim_test.h"
-#include "skinned.h"
+#include "debug_overlay.h"
 
-// Set this to 1 to enable rdpq debug output.
-// The demo will only run for a single frame and stop.
-#define DEBUG_RDP 0
+#define TICKRATE   60
+#define DELTATIME  (1.0f/(double)TICKRATE)
 
-static uint32_t animation = 3283;
-static uint32_t texture_index = 0;
-static camera_t camera;
+#define STICK_DEADZONE       10
+#define IGNORE_DEADZONE(v)   ((v) > STICK_DEADZONE || (v) < -STICK_DEADZONE ? (v) : 0)
 
-static uint64_t frames = 0;
+#define CAMERA_YAW_SPEED            0.015f
+#define CAMERA_PITCH_SPEED          0.015f
+#define CAMERA_DISTANCE_SPEED       0.05f
+#define CAMERA_DISTANCE_SPEED_FAST  0.5f
+#define CAMERA_MIN_PITCH            (-M_PI_2 * 0.9)
+#define CAMERA_MAX_PITCH            (M_PI_2 * 0.9)
 
-static GLuint textures[4];
+#define TEXTURE_COUNT 4
 
-static GLenum shade_model = GL_SMOOTH;
-static bool fog_enabled = false;
+static double subtick;
+
+static surface_t *zbuffer;
 
 static const GLfloat environment_color[] = { 0.1f, 0.03f, 0.2f, 1.f };
 
-static const GLfloat light_pos[8][4] = {
-    { 1, 0, 0, 0 },
-    { -1, 0, 0, 0 },
-    { 0, 0, 1, 0 },
-    { 0, 0, -1, 0 },
-    { 8, 3, 0, 1 },
-    { -8, 3, 0, 1 },
-    { 0, 3, 8, 1 },
-    { 0, 3, -8, 1 },
-};
+static fm_vec3_t camera_position = {{4.47f * 0.051812f, 134.180f * 0.051812f, 124.243f * 0.051812f}};
+static float camera_yaw = 0.030796327f;
+static float camera_pitch = -0.908407346f;
 
-static const GLfloat light_diffuse[8][4] = {
-    { 1.0f, 0.0f, 0.0f, 1.0f },
-    { 0.0f, 1.0f, 0.0f, 1.0f },
-    { 0.0f, 0.0f, 1.0f, 1.0f },
-    { 1.0f, 1.0f, 0.0f, 1.0f },
-    { 1.0f, 0.0f, 1.0f, 1.0f },
-    { 0.0f, 1.0f, 1.0f, 1.0f },
-    { 1.0f, 1.0f, 1.0f, 1.0f },
-    { 1.0f, 1.0f, 1.0f, 1.0f },
-};
+static uint64_t frames = 0;
+static bool display_metrics = false;
+static bool request_display_metrics = false;
+static float last_3d_fps = 0.0f;
 
-static const char *texture_path[4] = {
-    "rom:/circle0.sprite",
+static const char *texture_path[TEXTURE_COUNT] = {
+    "rom:/unit1m.i8.sprite",
     "rom:/diamond0.sprite",
     "rom:/pentagon0.sprite",
     "rom:/triangle0.sprite",
 };
 
-static sprite_t *sprites[4];
+static sprite_t *sprites[TEXTURE_COUNT];
+static GLuint textures[TEXTURE_COUNT];
 
-void setup()
+static model64_t *model;
+
+static float animation;
+
+GLuint test_list;
+
+void init()
 {
-    camera.distance = -10.0f;
-    camera.rotation = 0.0f;
+	debug_init(DEBUG_FEATURE_LOG_ISVIEWER | DEBUG_FEATURE_LOG_USB);
+    dfs_init(DFS_DEFAULT_LOCATION);
+    joypad_init();
+    resolution_t resolution = RESOLUTION_320x240;
+    display_init(resolution, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE_ANTIALIAS_DEDITHER);
+    zbuffer = display_get_zbuf();
+    gl_init();
+    //rdpq_debug_start();
+    //rdpq_debug_log(true);
 
-    for (uint32_t i = 0; i < 4; i++)
+    rspq_profile_start();
+    debug_overlay_init();
+
+    glGenTextures(TEXTURE_COUNT, textures);
+    for (uint32_t i = 0; i < TEXTURE_COUNT; i++)
     {
         sprites[i] = sprite_load(texture_path[i]);
+        glBindTexture(GL_TEXTURE_2D, textures[i]);
+        glSpriteTextureN64(GL_TEXTURE_2D, sprites[i], &(rdpq_texparms_t){.s.repeats = REPEAT_INFINITE, .t.repeats = REPEAT_INFINITE});
     }
-
-    setup_sphere();
-    make_sphere_mesh();
-
-    setup_cube();
-
-    setup_plane();
-    make_plane_mesh();
-
-    float aspect_ratio = (float)display_get_width() / (float)display_get_height();
-    float near_plane = 1.0f;
-    float far_plane = 50.0f;
 
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-near_plane*aspect_ratio, near_plane*aspect_ratio, -near_plane, near_plane, near_plane, far_plane);
+    float aspect_ratio = (float)resolution.width / (float)resolution.height;
+    gluPerspective(85.f, aspect_ratio, 1.f, 100.f);
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+    float ambient[4] = {0};
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
 
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, environment_color);
-    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+    float light_radius = 30.0f;
+    float light_color[4] = { 0.5f, 0.7f, 0.2f, 1.0f };
 
-    float light_radius = 10.0f;
+    //glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_color);
+    glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 1.0f/(light_radius*light_radius));
 
-    for (uint32_t i = 0; i < 8; i++)
-    {
-        glEnable(GL_LIGHT0 + i);
-        glLightfv(GL_LIGHT0 + i, GL_DIFFUSE, light_diffuse[i]);
-        glLightf(GL_LIGHT0 + i, GL_LINEAR_ATTENUATION, 2.0f/light_radius);
-        glLightf(GL_LIGHT0 + i, GL_QUADRATIC_ATTENUATION, 1.0f/(light_radius*light_radius));
-    }
+    glEnable(GL_LIGHT4);
+    float light_color2[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    glLightfv(GL_LIGHT4, GL_DIFFUSE, light_color2);
 
     GLfloat mat_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, mat_diffuse);
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 
-    glFogf(GL_FOG_START, 5);
-    glFogf(GL_FOG_END, 20);
+    glEnable(GL_FOG);
+    glFogf(GL_FOG_START, 20);
+    glFogf(GL_FOG_END, 90);
     glFogfv(GL_FOG_COLOR, environment_color);
 
-    glEnable(GL_MULTISAMPLE_ARB);
+    rdpq_mat_set_texture_path("rom:/");
+    model = model64_load("rom:/scene.model64");
+}
 
-    glGenTextures(4, textures);
+void direction_from_pitch_yaw(fm_vec3_t *out, float pitch, float yaw)
+{
+    float ys, yc, ps, pc;
+    fm_sincosf(yaw, &ys, &yc);
+    fm_sincosf(pitch, &ps, &pc);
+    *out = (fm_vec3_t){{
+        -pc * ys,
+        ps,
+        -pc * yc
+    }};
+}
 
-    #if 0
-    GLenum min_filter = GL_LINEAR_MIPMAP_LINEAR;
-    #else
-    GLenum min_filter = GL_LINEAR;
-    #endif
+void update_fixed(float deltatime)
+{
+    joypad_inputs_t inputs = joypad_get_inputs(JOYPAD_PORT_1);
 
+    int8_t stick_x = IGNORE_DEADZONE(inputs.stick_x);
+    int8_t stick_y = IGNORE_DEADZONE(inputs.stick_y);
+    int8_t cstick_x = IGNORE_DEADZONE(inputs.cstick_x);
+    int8_t cstick_y = IGNORE_DEADZONE(inputs.cstick_y);
 
-    for (uint32_t i = 0; i < 4; i++)
-    {
-        glBindTexture(GL_TEXTURE_2D, textures[i]);
+    camera_yaw -= cstick_x * deltatime * CAMERA_YAW_SPEED;
+    camera_pitch -= cstick_y * deltatime * CAMERA_PITCH_SPEED;
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min_filter);
+    if (camera_yaw > M_TWOPI) camera_yaw -= M_TWOPI;
+    if (camera_yaw < 0.0f) camera_yaw += M_TWOPI;
 
-        glSpriteTextureN64(GL_TEXTURE_2D, sprites[i], &(rdpq_texparms_t){.s.repeats = REPEAT_INFINITE, .t.repeats = REPEAT_INFINITE});
+    if (camera_pitch > CAMERA_MAX_PITCH) camera_pitch = CAMERA_MAX_PITCH;
+    if (camera_pitch < CAMERA_MIN_PITCH) camera_pitch = CAMERA_MIN_PITCH;
+
+    float distance_speed = deltatime * (inputs.btn.z ? CAMERA_DISTANCE_SPEED : CAMERA_DISTANCE_SPEED_FAST);
+
+    fm_vec3_t up = {{0, 1, 0}};
+    fm_vec3_t right;
+    fm_vec3_t forward;
+    direction_from_pitch_yaw(&forward, camera_pitch, camera_yaw);
+    fm_vec3_cross(&right, &forward, &up);
+    fm_vec3_scale(&forward, &forward, stick_y);
+    fm_vec3_scale(&right, &right, stick_x);
+    fm_vec3_add(&forward, &forward, &right);
+    fm_vec3_scale(&forward, &forward, distance_speed);
+    fm_vec3_add(&camera_position, &camera_position, &forward);
+
+    if (inputs.btn.a) {
+        animation += deltatime;
     }
 }
 
-void set_light_positions(float rotation)
+void update(float deltatime)
 {
-    glPushMatrix();
-    glRotatef(rotation*5.43f, 0, 1, 0);
+    joypad_buttons_t btn = joypad_get_buttons_pressed(JOYPAD_PORT_1);
 
-    for (uint32_t i = 0; i < 8; i++)
-    {
-        glLightfv(GL_LIGHT0 + i, GL_POSITION, light_pos[i]);
+    // L toggles the debug/profiler overlay on/off
+    if (btn.l) {
+        request_display_metrics = !request_display_metrics;
+        if (!request_display_metrics) display_metrics = false;
     }
-    glPopMatrix();
-}
 
-void render()
-{
-    surface_t *disp = display_get();
-    surface_t *zbuf = display_get_zbuf();
+    if (btn.start) {
+        debugf("x:%f, y:%f, z:%f, yaw:%f, pitch:%f\n", camera_position.x, camera_position.y, camera_position.z, camera_yaw, camera_pitch);
+    }
 
-    rdpq_attach(disp, zbuf);
+    surface_t *framebuffer = display_get();
+    rdpq_attach(framebuffer, zbuffer);
 
     gl_context_begin();
 
     glClearColor(environment_color[0], environment_color[1], environment_color[2], environment_color[3]);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    fm_vec3_t forward;
+    direction_from_pitch_yaw(&forward, camera_pitch, camera_yaw);
+    fm_vec3_add(&forward, &camera_position, &forward);
+
     glMatrixMode(GL_MODELVIEW);
-    camera_transform(&camera);
+    glLoadIdentity();
+    gluLookAt(
+        camera_position.x, camera_position.y, camera_position.z,
+        forward.x, forward.y, forward.z,
+        0.f, 1.f, 0.f);
 
-    float rotation = animation * 0.5f;
+    fm_vec4_t light_position = {{ 0, 10, 0, 1 }};
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position.v);
+    fm_vec4_t light_position2 = {{ -0.196116f, 0.784465f, 0.588348f, 0.0f }};
+    glLightfv(GL_LIGHT4, GL_POSITION, light_position2.v);
 
-    set_light_positions(rotation);
-
-    // Set some global render modes that we want to apply to all models
-    glEnable(GL_LIGHTING);
-    glEnable(GL_NORMALIZE);
+    //glEnable(GL_MULTISAMPLE_ARB);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
-
+    glEnable(GL_LIGHTING);
     glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, textures[texture_index]);
-    
-    render_plane();
-    render_decal();
-    render_cube();
-    render_skinned(&camera, animation);
+    //glEnable(GL_NORMALIZE);
 
-    glBindTexture(GL_TEXTURE_2D, textures[(texture_index + 1)%4]);
-    render_sphere(rotation);
-
-    glDisable(GL_TEXTURE_2D);
-    glDisable(GL_LIGHTING);
-    render_primitives(rotation);
-
-    // Draw a primitive with GL_RDPQ_TEXTURING_N64
-    glEnable(GL_RDPQ_TEXTURING_N64);
-    glEnable(GL_RDPQ_MATERIAL_N64);
-
-    // When rendering with GL_RDPQ_TEXTURING_N64 we need to manualy specify the
-    // tile size and if a 0.5 offset should be used since the ucode itself cannot
-    // determine these. Here we set the tile size to be 32x32 and we apply an offset
-    // since we are using bilinear texture filtering
-    glTexSizeN64(32, 32);
-    rdpq_sprite_upload(TILE0, sprites[0], &(rdpq_texparms_t){.s.repeats = REPEAT_INFINITE, .t.repeats = REPEAT_INFINITE});
-    rdpq_set_mode_standard();
-    rdpq_mode_filter(FILTER_BILINEAR);
-
-    glBegin(GL_TRIANGLE_FAN);
-        glTexCoord2f(0.0f, 0.0f);
-        glVertex3f(-5.5f, 1.0f, -1.0f);
-
-        glTexCoord2f(0.0f, 1.0f);
-        glVertex3f(-5.5f, 1.0f, 1.0f);
-
-        glTexCoord2f(1.0f, 1.0f);
-        glVertex3f(-3.5f, 1.0f, 1.0f);
-
-        glTexCoord2f(1.0f, 0.0f);
-        glVertex3f(-3.5f, 1.0f, -1.0f);
-    glEnd();
-
-    glDisable(GL_RDPQ_TEXTURING_N64);
-    glDisable(GL_RDPQ_MATERIAL_N64);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    rdpq_set_prim_color(color_from_packed32(0xFFFFFFFF));
+    model64_draw(model);
 
     gl_context_end();
+
+    if (display_metrics) {
+        debug_draw_perf_overlay(last_3d_fps);
+    }
 
     rdpq_detach_show();
 
     rspq_profile_next_frame();
 
-    if (((frames++) % 60) == 0) {
-        rspq_profile_dump();
+    if (frames == 30) {
+        if (!display_metrics) {
+            last_3d_fps = display_get_fps();
+            rspq_wait();
+            rspq_profile_get_data(&profile_data);
+            if (request_display_metrics) display_metrics = true;
+        }
+
+        frames = 0;
         rspq_profile_reset();
-        debugf("frame %lld\n", frames);
     }
+
+    frames++;
 }
 
 int main()
 {
-	debug_init_emulog();
-	debug_init_usblog();
-    
-    dfs_init(DFS_DEFAULT_LOCATION);
+    init();
 
-    display_init(RESOLUTION_320x240, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE_ANTIALIAS_DEDITHER);
+    float accumulator = 0;
+    const float dt = DELTATIME;
 
-    rdpq_init();
-    gl_init();
-
-#if DEBUG_RDP
-    rdpq_debug_start();
-    rdpq_debug_log(true);
-#endif
-
-    setup();
-
-    joypad_init();
-
-    rspq_profile_start();
-
-#if !DEBUG_RDP
-    while (1)
-#endif
+    while (true)
     {
         joypad_poll();
-        joypad_buttons_t pressed = joypad_get_buttons_pressed(JOYPAD_PORT_1);
-        joypad_buttons_t held = joypad_get_buttons_held(JOYPAD_PORT_1);
-        joypad_inputs_t inputs = joypad_get_inputs(JOYPAD_PORT_1);
-
-        if (held.a) {
-            animation++;
+        
+        float frametime = display_get_delta_time();
+        
+        // In order to prevent problems if the game slows down significantly, we will clamp the maximum timestep the simulation can take
+        if (frametime > 0.25f)
+            frametime = 0.25f;
+        
+        // Perform the update in discrete steps (ticks)
+        accumulator += frametime;
+        while (accumulator >= dt)
+        {
+            update_fixed(dt);
+            accumulator -= dt;
         }
 
-        if (held.b) {
-            animation--;
-        }
-
-        if (pressed.start) {
-            debugf("%ld\n", animation);
-        }
-
-        if (pressed.r) {
-            shade_model = shade_model == GL_SMOOTH ? GL_FLAT : GL_SMOOTH;
-            glShadeModel(shade_model);
-        }
-
-        if (pressed.l) {
-            fog_enabled = !fog_enabled;
-            if (fog_enabled) {
-                glEnable(GL_FOG);
-            } else {
-                glDisable(GL_FOG);
-            }
-        }
-
-        if (pressed.c_up) {
-            if (sphere_rings < SPHERE_MAX_RINGS) {
-                sphere_rings++;
-            }
-
-            if (sphere_segments < SPHERE_MAX_SEGMENTS) {
-                sphere_segments++;
-            }
-
-            make_sphere_mesh();
-        }
-
-        if (pressed.c_down) {
-            if (sphere_rings > SPHERE_MIN_RINGS) {
-                sphere_rings--;
-            }
-
-            if (sphere_segments > SPHERE_MIN_SEGMENTS) {
-                sphere_segments--;
-            }
-            
-            make_sphere_mesh();
-        }
-
-        if (pressed.c_right) {
-            texture_index = (texture_index + 1) % 4;
-        }
-
-        float y = inputs.stick_y / 128.f;
-        float x = inputs.stick_x / 128.f;
-        float mag = x*x + y*y;
-
-        if (fabsf(mag) > 0.01f) {
-            camera.distance += y * 0.2f;
-            camera.rotation = camera.rotation - x * 1.2f;
-        }
-
-        render();
-        if (DEBUG_RDP)
-            rspq_wait();
+        subtick = ((double)accumulator)/((double)dt);
+        update(frametime);
     }
-
 }
--- src/video/libdragon.mk
+++ src/video/libdragon.mk
@@ -6,8 +6,4 @@ LIBDRAGON_OBJS += \
 	$(BUILD_DIR)/video/yuv.o \
 	$(BUILD_DIR)/video/rsp_yuv.o \
 	$(BUILD_DIR)/video/rsp_mpeg1.o \
-	$(BUILD_DIR)/video/h264_decoder.o \
-	$(BUILD_DIR)/video/h264.o \
-	$(BUILD_DIR)/video/rsph264_inter.o \
-	$(BUILD_DIR)/video/rsph264_intra.o \
 	$(BUILD_DIR)/video/subtitles.o
